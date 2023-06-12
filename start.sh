02e2277b8abb15683769b4f6b3fb015e3598f3c3
Pseud0R4ndom <94747795+wittdennis@users.noreply.github.com>
2023-06-12 11:35:21 +0200

    remove old docker compose start script (#6)

--- start.sh
+++
@@ -1,9 +0,0 @@
-# Absolute path to this script, e.g. /home/user/bin/foo.sh
-SCRIPT=$(readlink -f "$0")
-# Absolute path this script is in, thus /home/user/bin
-SCRIPTPATH=$(dirname "$SCRIPT")
-
-mkdir -p /var/ts3server
-mkdir -p /var/ts3db
-mkdir -p /var/run/mysqld
-docker compose -f $SCRIPTPATH/src/docker-compose.yml -p ts-server up --detach
